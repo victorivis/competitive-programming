79d58714c5f3798af9fd5a7979056222369b1483
victorivis <knightdragonred@gmail.com>
2025-04-20 20:50:51 -0300

    A. Cloudberry Jar - 800 - math

---
+++ zerando-codeforces/Educational/Educational Codeforces Round 177/A. Cloudberry Jam.cpp
@@ -0,0 +1,20 @@
+/*
+https://codeforces.com/contest/2086/problem/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        
+        cout << 2*n << "\n";
+    }
+    
+    return 0;
+}
