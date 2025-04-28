db411e5d4f11940b1661cabe8dbd5688245b1732
victorivis <knightdragonred@gmail.com>
2025-04-28 15:38:31 -0300

    A. Maximum GCD - 800 - greedy, number theory

---
+++ zerando-codeforces/Topic Stream/Topic Stream Mashup Number Theory/A. Maximum GCD.cpp
@@ -0,0 +1,14 @@
+/* https://codeforces.com/gym/348180 */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int x; cin >> x;
+        cout << (x/2) << "\n";
+    }
+ 
+    return 0;
+}
