f872878e8b0a1fccfb00397fecb286145d180605
victorivis <knightdragonred@gmail.com>
2025-04-28 15:47:50 -0300

    B. EhAb AnD gCd - 800 - constructive algorithms, number theory

---
+++ zerando-codeforces/Topic Stream/Topic Stream Mashup Number Theory/B. EhAb AnD gCd.cpp
@@ -0,0 +1,14 @@
+/* https://codeforces.com/gym/348180/problem/B */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        cout << 1 << " " <<  n-1 << "\n";
+    }
+
+    return 0;
+}
