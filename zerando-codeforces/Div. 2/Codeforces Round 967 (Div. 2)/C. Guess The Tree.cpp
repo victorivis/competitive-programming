bd1611d4489359576ef18566d54031156f215239
Victor Ivis <109966871+victorivis@users.noreply.github.com>
2026-09-19 17:15:04 -0300

    C. Guess The Tree - 1500 - interactive, binary search, dsu, greedy, trees

---
+++ zerando-codeforces/Div. 2/Codeforces Round 967 (Div. 2)/C. Guess The Tree.cpp
@@ -0,0 +1,116 @@
+// https://codeforces.com/contest/2001/problem/C
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define is insert
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique(x) (x).erase(unique((x).begin(), (x).end()), (x).end())
+#define f(i,b,e) for (int i = (b); i < (e); ++i)
+#define rf(i,b,e) for (int i = (b); i >= (e); --i)
+#define fa(i,a) for (auto& i : (a))
+#define sz(x) ((int)(x).size())
+#define fi first
+#define se second
+#define mkp make_pair
+#define mark if(info)
+
+using namespace std;
+using namespace __gnu_pbds;
+
+using par = array<int,2>;
+using ii = pair<int,int>;
+using iii = array<int,3>;
+
+bool info = true;
+
+template<class T> concept It = requires(T t){begin(t);end(t);} && !is_same_v<T,string>;
+template<class T> struct is_arr : false_type {};
+template<class T, size_t N> struct is_arr<array<T,N>> : true_type {};
+
+template<class T> void pv(const T& v){
+    if constexpr (is_arr<T>::value) { cout<<"("; for(size_t i=0;i<v.size();++i){ pv(v[i]); cout<<" )"[i+1==v.size()]; } }
+    else if constexpr (requires{v.first; v.second;}) { cout<<"("; pv(v.first); cout<<" "; pv(v.second); cout<<")"; }
+    else if constexpr (It<T>) { cout<<"["; bool f=1; for(auto&x:v){ if(!f) cout<<", "; f=0; pv(x); } cout<<"]"; }
+    else cout<<v;
+}
+
+template<class T, class... A>
+void dbo(const char* n, T v, A... a){
+    if(!info) return;
+    const char* c = strchr(n,',');
+    cout.write(n, c ? c-n : strlen(n)) << "=";
+    pv(v);
+    if constexpr (sizeof...(a)) cout<<" ", dbo(c+1, a...);
+    else cout<<"\n";
+}
+
+#define dbg(...) dbo(#__VA_ARGS__, __VA_ARGS__)
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+bool all_test = 1;
+
+const int MAXN = 2e5 + 1, MOD = 1e9 + 7, MODW = 998244353, oo = 1ll << 60;
+
+const ld pi = acos(-1.0);
+
+int query(int a, int b){
+    cout << "? " << a << ' ' << b << endl;
+    int x; cin >> x;
+    return x;
+}
+
+set<int> st;
+vector<ii> ans;
+int rec(int l, int r){
+    assert(st.count(l)!=0 and st.count(r)==0);
+
+    while(true){
+        int val = query(l,r);        
+        if(val==l or val==r) break;
+        if(st.count(val)) l = val;
+        else r = val;
+    };
+
+    ans.pb({l,r});
+    return r;
+}
+
+void solve() {
+    st.clear();
+    ans.clear();
+
+    int n; cin >> n;
+    st.insert(1);
+
+    auto rng = views::iota(2ll,n+1);
+    set<int> falta(all(rng));
+    
+    int lst=1;
+    while(falta.empty()==false){
+        int sai = rec(lst,*falta.begin());        
+        falta.erase(sai);
+        st.insert(sai);
+    }
+    
+    cout << "! ";
+    fa(x,ans){
+        cout << x.fi << " " << x.se << " ";
+    }
+    cout << endl;
+}
+
+signed main() {
+    int tc = 1;
+    if (all_test) cin >> tc;
+    while (tc--) solve();
+    return 0;
+}
