b037a92872bc73d078729f21fe046566443adcd8
Victor Ivis <109966871+victorivis@users.noreply.github.com>
2026-09-24 10:09:51 -0300

    E. MEX Count - 1400 - binary search, data structures, greedy, sortings, two pointers
    a bit funny

---
+++ zerando-codeforces/Div. 3/Codeforces Round 1034 (Div. 3)/E. MEX Count.cpp
@@ -0,0 +1,119 @@
+// https://codeforces.com/contest/2123/problem/E
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define is insert
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique(x) (x).erase(unique((x).begin(), (x).end()), (x).end())
+#define f(i,b,e) for (int i = (b); i < (e); ++i)
+#define rf(i,b,e) for (int i = (b); i >= (e); --i)
+#define fa(i,a) for (auto& i : (a))
+#define sz(x) ((int)(x).size())
+#define fi first
+#define se second
+#define mkp make_pair
+#define mark if(info)
+#define fit(x) for(auto it=(x).begin(); it!=(x).end(); it++)
+
+using namespace std;
+using namespace __gnu_pbds;
+
+using par = array<int,2>;
+using ii = pair<int,int>;
+using iii = array<int,3>;
+
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vector<int>> vvi;
+typedef set<int> si;
+typedef map<int,int> mii;
+
+bool info = true;
+
+template<class T> concept It = requires(T t){begin(t);end(t);} && !is_same_v<T,string>;
+template<class T> struct is_arr : false_type {};
+template<class T, size_t N> struct is_arr<array<T,N>> : true_type {};
+
+template<class T> void pv(const T& v){
+    if constexpr (is_arr<T>::value) { cout<<"("; for(size_t i=0;i<v.size();++i){ pv(v[i]); cout<<" )"[i+1==v.size()]; } }
+    else if constexpr (requires{v.first; v.second;}) { cout<<"("; pv(v.first); cout<<" "; pv(v.second); cout<<")"; }
+    else if constexpr (It<T>) { cout<<"["; bool f=1; for(auto&x:v){ if(!f) cout<<", "; f=0; pv(x); } cout<<"]"; }
+    else cout<<v;
+}
+
+template<class T, class... A>
+void dbo(const char* n, T v, A... a){
+    if(!info) return;
+    const char* c = strchr(n,',');
+    cout.write(n, c ? c-n : strlen(n)) << "=";
+    pv(v);
+    if constexpr (sizeof...(a)) cout<<" ", dbo(c+1, a...);
+    else cout<<"\n";
+}
+
+#define dbg(...) dbo(#__VA_ARGS__, __VA_ARGS__)
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+bool all_test = 1;
+
+const int MAXN = 2e5 + 1, MOD = 1e9 + 7, MODW = 998244353, oo = 1ll << 60;
+
+const ld pi = acos(-1.0);
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+
+    map<int,int> d;
+    f(i,0,n){
+        cin >> a[i];
+        d[a[i]]++;
+    }
+
+    auto func = [&](vector<int>& a) -> int {
+        set<int> st(all(a));
+        int pos = 0;
+        while(st.count(pos)){
+            pos++;
+        }
+        return pos;
+    };
+    int mex = func(a);
+
+    vector<int> ans(n+3);
+    f(cur,0,mex+1){
+        int quant_atual = d[cur];
+
+        int mn = quant_atual;
+        int mx = n-cur;
+        ans[mn]+=1;
+        ans[mx+1]-=1;
+    }
+
+    f(i,1,ans.size()){
+        ans[i] += ans[i-1];
+    }
+
+    f(i,0,n+1){
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    int tc = 1;
+    if (all_test) cin >> tc;
+    while (tc--) solve();
+    return 0;
+}
